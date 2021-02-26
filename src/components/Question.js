f87910ee543626e11b5aed907a9f220ce5016e0e
Kody Samaroo <github email address>
2021-02-25 21:02:14 -0500

    Done.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+    useEffect(() => {
+    if (timeRemaining === 0){
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+    const timerID = setTimeout(() => setTimeRemaining((timeRemaining) => timeRemaining - 1), 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearTimeout(timerID);
+    };
+  }, [timeRemaining, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
